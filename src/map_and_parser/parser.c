f2eb82a64a05c4b0965f8b423cb5ba3b5815653a
dkalaxdk <alax0013@gmail.com>
2018-12-04 17:50:18 +0100

    Parser function seems to be working

--- src/map_and_parser/parser.c
+++ src/map_and_parser/parser.c
@@ -4,19 +4,17 @@
 #include "cJSON.c"
 #include "cJSON.h"
 
-int lines_counter();
-
 void convert_to_array(char *text);
 
 void parse_file();
 
 struct tags {
     char city[30];
+    char street[30];
     char country[5];
     char house_number[5];
     char muncipality[20];
     char postcode[4];
-    char street[30];
 };
 
 struct address {
@@ -29,14 +27,14 @@ struct address {
 
 struct address parser(struct address curr_address, FILE *paddress_file, char search[], int line);
 
+int is_in_array(char input_streetname[], char input_housenumber[]);
+
 int main() {
 
     FILE *paddress_file;
     paddress_file = fopen("addresses.json", "rb");
 
     parse_file();
-    int lines = lines_counter(paddress_file);
-    printf("Number of lines: %d \n", lines);
 
     if (paddress_file == NULL) {
         printf("ERROR ON LOADING FILE");
@@ -80,15 +78,8 @@ void convert_to_array(char *text) {
     const cJSON *element = NULL;
     const cJSON *elements = NULL;
     cJSON *tags = NULL;
-    char *test_string;
-    struct address *addresses;
-    int i = 1, j = 0;
-    char test[50];
-    int lines = lines_counter();
-    char *search_street= "Abelsvej";
-    char search_number[4];
-
-    addresses = calloc(lines, sizeof(address));
+    struct address addresses[20];
+    int j = 0;
 
     json = cJSON_Parse(text);
 
@@ -98,80 +89,68 @@ void convert_to_array(char *text) {
     } else {
         elements = cJSON_GetObjectItemCaseSensitive(json, "elements");
         cJSON_ArrayForEach(element, elements) {
-            //printf("Adresses %s Run: %d \n",addresses[i].tags.street,i);
-            addresses[i].lat = cJSON_GetObjectItem(element, "lat")->valuedouble;
-            addresses[i].lon = cJSON_GetObjectItem(element, "lon")->valuedouble;
             tags = cJSON_GetObjectItemCaseSensitive(element, "tags");
 
-            strcpy(addresses[i].tags.street, cJSON_GetObjectItemCaseSensitive(tags, "addr:street")->valuestring);
-            strcpy(addresses[i].tags.country, cJSON_GetObjectItemCaseSensitive(tags, "addr:country")->valuestring);
-            strcpy(addresses[i].tags.muncipality,
-                   cJSON_GetObjectItemCaseSensitive(tags, "addr:municipality")->valuestring);
-            strcpy(addresses[i].tags.city, cJSON_GetObjectItemCaseSensitive(tags, "addr:city")->valuestring);
-            strcpy(addresses[i].tags.house_number,
-                   cJSON_GetObjectItemCaseSensitive(tags, "addr:housenumber")->valuestring);
-            strcpy(addresses[i].tags.postcode, cJSON_GetObjectItemCaseSensitive(tags, "addr:postcode")->valuestring);
-            /*printf("\n Streetname %s \n Muncipality %s \n Country %s \n City %s \n HouseNumber %s \n Postcode %s \n",
-                   addresses[i].tags.street, addresses[i].tags.muncipality, addresses[i].tags.country,
-                   addresses[i].tags.city, addresses[i].tags.house_number, addresses[i].tags.postcode);
-            printf("lateral: %f \n", addresses[i].lat); */
-        }
-    }
-    strcpy(search_number, "4");
-    for (int j = 0; j < lines; ++j) {
-        if (strcmp(search_street, addresses[j].tags.street) == 0) {
-            if (strcmp(search_number, addresses[j].tags.house_number) == 0) {
-                printf("Latitude %f \n "
-                       "Longtitude %f \n", addresses[j].lat, addresses[j].lon);
-            } else {
-                printf("Street found, not right address \n");
+            if (is_in_array(cJSON_GetObjectItemCaseSensitive(tags, "addr:street")->valuestring,
+                            cJSON_GetObjectItemCaseSensitive(tags, "addr:housenumber")->valuestring) == 1) {
+
+                addresses[j].lat = cJSON_GetObjectItem(element, "lat")->valuedouble;
+                addresses[j].lon = cJSON_GetObjectItem(element, "lon")->valuedouble;
+
+                strcpy(addresses[j].tags.street,
+                       cJSON_GetObjectItemCaseSensitive(tags, "addr:street")->valuestring);
+                strcpy(addresses[j].tags.country,
+                       cJSON_GetObjectItemCaseSensitive(tags, "addr:country")->valuestring);
+                strcpy(addresses[j].tags.muncipality,
+                       cJSON_GetObjectItemCaseSensitive(tags, "addr:municipality")->valuestring);
+                strcpy(addresses[j].tags.city,
+                       cJSON_GetObjectItemCaseSensitive(tags, "addr:city")->valuestring);
+                strcpy(addresses[j].tags.house_number,
+                       cJSON_GetObjectItemCaseSensitive(tags, "addr:housenumber")->valuestring);
+
+                strcpy(addresses[j].tags.postcode,
+                       cJSON_GetObjectItemCaseSensitive(tags, "addr:postcode")->valuestring);
+
+                printf("Succes you git: Streetnam: %s \n Housenumber %s \n "
+                       "Long: %lf \n"
+                       "Lat: %lf \n", addresses[j].tags.street,
+                       addresses[j].tags.house_number,addresses[j].lon,addresses[j].lat);
+                j++;
             }
-        } else {
-             printf("Reached end, no such thing \n ");
         }
     }
 }
+int is_in_array(char input_streetname[], char input_housenumber[]) {
+    struct address searches[20];
+    strcpy(searches[0].tags.street, "Abelsvej");
+    strcpy(searches[0].tags.house_number, "2");
+    strcpy(searches[1].tags.street, "James Tobins Alle");
+    strcpy(searches[1].tags.house_number, "2");
+    strcpy(searches[2].tags.street, "Jernbanegade");
+    strcpy(searches[2].tags.house_number, "10");
+    strcpy(searches[3].tags.street, "Borgmestervæeget");
+    strcpy(searches[3].tags.house_number, "1");
+    strcpy(searches[4].tags.street, "Vesterled");
+    strcpy(searches[4].tags.house_number, "22");
+    strcpy(searches[5].tags.street, "Kastetvej");
+    strcpy(searches[5].tags.house_number, "117");
+    strcpy(searches[6].tags.street, "Bobsvinvej");
+    strcpy(searches[6].tags.house_number, "2");
+    strcpy(searches[7].tags.street, "Brandstrupsgade");
+    strcpy(searches[7].tags.house_number, "10");
+    strcpy(searches[8].tags.street, "Danmarksgade");
+    strcpy(searches[8].tags.house_number, "49");
+    strcpy(searches[9].tags.street, "Fyensgade");
+    strcpy(searches[9].tags.house_number, "54");
+    strcpy(searches[10].tags.street, "Herskindsgade");
+    strcpy(searches[10].tags.house_number, "20");
 
-int lines_counter() {
-    FILE *paddress_file;
-    paddress_file = fopen("addresses.json", "rb");
-    int lines = 0;
-    while (!feof(paddress_file)) {
-        char c = fgetc(paddress_file);
-        if (c == '\n') {
-            lines++;
+    for (int k = 0; k < 10; ++k) {
+        if (strcmp(input_streetname, searches[k].tags.street) == 0) {
+            if (strcmp(input_housenumber, searches[k].tags.house_number) == 0) {
+                return 1;
+            }
         }
     }
-    return lines;
+    return 0;
 }
-
-
-/*struct address parser(struct address curr_address, FILE *paddress_file, char search[], int line) {
-    fscanf(paddress_file, "      \"type\": \"node\",\n"
-                          "      \"id\": %d,\n"
-                          "      \"lat\": %lf,\n"
-                          "      \"lon\": %lf,\n"
-                          "      \"tags\": {\n"
-                          "        \"addr:city\": \" %[a-zA-Z ] \",\n"
-                          "        \"addr:country\": \" %s\",\n"
-                          "        \"addr:housenumber\": \" %d\",\n"
-                          "        \"addr:municipality\": \" %[a-zA-Z ]\",\n"
-                          "        \"addr:postcode\": \" %d\",\n"
-                          "        \"addr:street\": \" %[a-zA-Z ] \"", &curr_address.id, &curr_address.lat,
-           &curr_address.lon, curr_address.city, curr_address.country, &curr_address.house_number,
-           curr_address.muncipality, &curr_address.postcode, curr_address.street);
-
-    //fscanf(paddress_file, "id\": %d,\n",&curr_address.id);
-    //fscanf(paddress_file, "lat\": %lf,\n",&curr_address.lat);
-    //fscanf(paddress_file, "lon\": %lf,\n",&curr_address.lat);
-    //fscanf(paddress_file, "addr:city\": \" %[a-zA-Z ]\",\n",curr_address.country);
-    //fscanf(paddress_file, "addr:country\": \" %s\",\n",curr_address.city);
-    //fscanf(paddress_file, "addr:housenumber\": \" %d \",\n",&curr_address.house_number);
-    //fscanf(paddress_file, "addr:municipality\": \" %s \",\n",curr_address.muncipality);
-    //fscanf(paddress_file, "addr:postcode\": \" %d\" ,\n",&curr_address.postcode);
-    //fscanf(paddress_file, "addr:street\": \" %[a-zA-Z ] \",\n\"",curr_address.street);
-
-    //printf("Test: %s",curr_address.street);
-    return curr_address;
-
-}*/
