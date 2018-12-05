5addd2f87c3d0334cb94d0c2904e613cc1f024c2
sejbas <sejbas@users.noreply.github.com>
2018-12-05 15:28:37 +0100

    Resize and add element have been worked on

--- src/dynamic_array.c
+++ src/dynamic_array.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+##include <math.h>
 
 
 dyn_array *resize_array(dyn_array *array, int new_size) {
-  array.low_water_mark = new_size/4;
+  array.low_water_mark = (int)ceil(new_size/4);
+  array.high_water_mark = new_size;
+  //array.size = new_size;
   array = realloc(array, new_size);
   if(array == NULL) {
     exit(EXIT_FAILURE);
@@ -12,11 +15,17 @@ dyn_array *resize_array(dyn_array *array, int new_size) {
 }
 
 dyn_array *add_element_to_end(dyn_array *array_to_insert_in, element element_to_insert) {
+  dyn_array *res = array_to_insert_in;
   /* If array is not able to hold another element then resize array */
   if(array_to_insert_in.size == array_to_insert_in.high_water_mark) {
     /* If array is resized then a pointer to the new array is returned */
-    return resize_array(array_to_insert_in, 2 * array_to_insert_in.size);
+    res = resize_array(array_to_insert_in, 2 * array_to_insert_in.size);
   }
+  ++array_to_insert_in.items;
   /* If array is not resized then NULL is returned */
   return NULL;
+}
+
+void ensure_capacity(dyn_array *array, int capacity) {
+  array.min_capacity = capacity;
 }
--- src/dynamic_array.h
+++ src/dynamic_array.h
@@ -2,21 +2,27 @@
 "A Practical Guide to Data Structures and Algorithms using Java" */
 
 struct dyn_array {
+  //The threshold for then the array is reduced in size
   int low_water_mark;
+  //The threshold for then the array is expanded in size
   int high_water_mark;
+  //The minimum size the array can have
   int min_capacity;
-  int size;
+  //Number of items in array
+  int items;
+  //The size of the array
+  //int size; maybe not needed
 };
 
 typedef dyn_array dyn_array;
 
 void trim_to_size();
 
-void ensure_capacity();
+void ensure_capacity(dyn_array *array, int capacity);
 
 dyn_array *resize_array(dyn_array *array, int new_size);
 
-void add_element_to_end(dyn_array *array_to_insert_in, element element_to_insert);
+dyn_array *add_element_to_end(dyn_array *array_to_insert_in, element element_to_insert);
 
 void remove_range_of_elements();
 
