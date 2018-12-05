86f165f5cd965f072771ead9121e22421f1e7edb
sejbas <sejbas@users.noreply.github.com>
2018-12-05 14:16:41 +0100

    dynamic array resize and add element is maybe done

--- src/dynamic_array.c
+++ src/dynamic_array.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
-
-dyn_array resize_array(dyn_array old_array, int new_size) {
-
+dyn_array *resize_array(dyn_array *array, int new_size) {
+  array.low_water_mark = new_size/4;
+  array = realloc(array, new_size);
+  if(array == NULL) {
+    exit(EXIT_FAILURE);
+  }
+  return array;
 }
 
-void add_element_to_end(dyn_array *array_to_insert_in, element element_to_insert) {
-
+dyn_array *add_element_to_end(dyn_array *array_to_insert_in, element element_to_insert) {
+  /* If array is not able to hold another element then resize array */
+  if(array_to_insert_in.size == array_to_insert_in.high_water_mark) {
+    /* If array is resized then a pointer to the new array is returned */
+    return resize_array(array_to_insert_in, 2 * array_to_insert_in.size);
+  }
+  /* If array is not resized then NULL is returned */
+  return NULL;
 }
--- src/map_and_parser/dynamic_array.h
+++ src/map_and_parser/dynamic_array.h
@@ -1,10 +1,11 @@
-/* dynamic_array.h - made with inspiration from book...*/
+/* dynamic_array.h - made with inspiration from
+"A Practical Guide to Data Structures and Algorithms using Java" */
 
 struct dyn_array {
   int low_water_mark;
   int high_water_mark;
   int min_capacity;
-  int number_of_elements;
+  int size;
 };
 
 typedef dyn_array dyn_array;
@@ -13,6 +14,10 @@ void trim_to_size();
 
 void ensure_capacity();
 
-dyn_array resize_array(dyn_array *old_array, int new_size);
+dyn_array resize_array(dyn_array *array, int new_size);
 
 void add_element_to_end(dyn_array *array_to_insert_in, element element_to_insert);
+
+void remove_range_of_elements();
+
+void find_element();
