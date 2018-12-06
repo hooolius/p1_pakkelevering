7f73073696c2c9504e7c21f1178454912b775bd0
sejbas <sejbas@users.noreply.github.com>
2018-12-06 20:46:12 +0100

    Made a implementation for heap and node

--- src/dynamic_array.c
+++ src/dynamic_array.c
@@ -6,8 +6,9 @@
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-dyn_array *make_dyn_array(int min_capacity, ) {
-  dyn_array *array = calloc(min_capacity, sizeof(dyn_array) + sizeof());
+/* HEAP FUNCTIONS */
+dyn_array_heap *make_dyn_array_h(int min_capacity) {
+  dyn_array_heap *array = calloc(min_capacity, sizeof(dyn_array_heap);
   array.min_capacity = min_capacity;
   array.low_water_mark = min_capacity;
   array.high_water_mark = 2 * min_capacity;
@@ -15,7 +16,7 @@ dyn_array *make_dyn_array(int min_capacity, ) {
   return array;
 }
 
-dyn_array *resize_array(dyn_array *array, int new_size) {
+dyn_array_heap *resize_array_h(dyn_array_heap *array, int new_size) {
   array.low_water_mark = (int)ceil(new_size/4);
   array.high_water_mark = new_size;
   //array.size = new_size;
@@ -26,8 +27,8 @@ dyn_array *resize_array(dyn_array *array, int new_size) {
   return array;
 }
 
-dyn_array *add_element_to_end(dyn_array *array_to_insert_in, element elem_to_insert) {
-  dyn_array *res = array_to_insert_in;
+dyn_array_heap *add_element_to_end_h(dyn_array_heap *array_to_insert_in, heap_elem heap_to_insert) {
+  dyn_array_heap *res = array_to_insert_in;
   ++items;
   /* If array is not able to hold another element then resize array */
   if(array_to_insert_in.items >= array_to_insert_in.high_water_mark) {
@@ -39,12 +40,12 @@ dyn_array *add_element_to_end(dyn_array *array_to_insert_in, element elem_to_ins
   return NULL;
 }
 
-void ensure_capacity(dyn_array *array, int capacity) {
+void ensure_capacity_h(dyn_array_heap *array, int capacity) {
   array.min_capacity = capacity;
 }
 
-dyn_array *delete_element(dyn_array *array, element elem_to_delete) {
-  dyn_array *res = array;
+dyn_array_heap *delete_element_h(dyn_array_heap *array, heap_elem heap_to_delete) {
+  dyn_array_heap *res = array;
   for (int i = 0; i < array.items; ++i) {
     if(array.elements[i] == elem_to_delete) {
       array.elements[i] = array.elements[items];
@@ -57,8 +58,70 @@ dyn_array *delete_element(dyn_array *array, element elem_to_delete) {
   return res;
 }
 
-dyn_array *find_element(dyn_array *array, element elem_to_find) {
-  dyn_array *res;
+dyn_array_heap *find_element_h(dyn_array_heap *array, heap_elem heap_to_find) {
+  dyn_array_heap *res;
+  for (int i = 0; i < array.items; ++i) {
+    if(array.elements[i] == elem_to_find) {
+      res = &array.elements[i];
+    }
+  }
+  return res;
+}
+
+/* NODE FUNCTIONS */
+dyn_array_node *make_dyn_array_n(int min_capacity, ) {
+  dyn_array_node *array = calloc(min_capacity, sizeof(dyn_array_node));
+  array.min_capacity = min_capacity;
+  array.low_water_mark = min_capacity;
+  array.high_water_mark = 2 * min_capacity;
+  array.items = 0;
+  return array;
+}
+
+dyn_array_node *resize_array_n(dyn_array_node *array, int new_size) {
+  array.low_water_mark = (int)ceil(new_size/4);
+  array.high_water_mark = new_size;
+  //array.size = new_size;
+  array = realloc(array, new_size);
+  if(array == NULL) {
+    exit(EXIT_FAILURE);
+  }
+  return array;
+}
+
+dyn_array_node *add_element_to_end_n(dyn_array_node *array_to_insert_in, node node_to_insert) {
+  dyn_array_node *res = array_to_insert_in;
+  ++items;
+  /* If array is not able to hold another element then resize array */
+  if(array_to_insert_in.items >= array_to_insert_in.high_water_mark) {
+    /* If array is resized then a pointer to the new array is returned */
+    res = resize_array(array_to_insert_in, 2 * array_to_insert_in.high_water_mark);
+  }
+  ++array_to_insert_in.items;
+  /* If array is not resized then NULL is returned */
+  return NULL;
+}
+
+void ensure_capacity_n(dyn_array_node *array, int capacity) {
+  array.min_capacity = capacity;
+}
+
+dyn_array_node *delete_element_n(dyn_array *array, node node_to_delete) {
+  dyn_array_node *res = array;
+  for (int i = 0; i < array.items; ++i) {
+    if(array.elements[i] == elem_to_delete) {
+      array.elements[i] = array.elements[items];
+      --items;
+    }
+  }
+  if(array.items < array.low_water_mark) {
+    res = resize_array(MAX(2*high_water_mark, array.min_capacity));
+  }
+  return res;
+}
+
+dyn_array_node *find_element_n(dyn_array_node *array, node node_to_find) {
+  dyn_array_node *res;
   for (int i = 0; i < array.items; ++i) {
     if(array.elements[i] == elem_to_find) {
       res = &array.elements[i];
--- src/dynamic_array.h
+++ src/dynamic_array.h
@@ -1,7 +1,7 @@
 /* dynamic_array.h - made with inspiration from
 "A Practical Guide to Data Structures and Algorithms using Java" */
 
-struct dyn_array {
+struct dyn_array_heap {
   //The threshold for then the array is reduced in size
   int low_water_mark;
   //The threshold for then the array is expanded in size
@@ -12,21 +12,54 @@ struct dyn_array {
   int items;
   //The size of the array
   //int size; maybe not needed
-  //element elements[];
+  heap_elem heap_elems[];
 };
 
-typedef struct dyn_array dyn_array;
+struct dyn_array_node {
+  //The threshold for then the array is reduced in size
+  int low_water_mark;
+  //The threshold for then the array is expanded in size
+  int high_water_mark;
+  //The minimum size the array can have
+  int min_capacity;
+  //Number of items in array
+  int items;
+  //The size of the array
+  //int size; maybe not needed
+  node nodes[];
+};
+
+typedef struct dyn_array_heap dyn_array_heap;
+typedef struct dyn_array_node dyn_array_node;
+
+/* HEAP FUNCTIONS */
 /* Makes a empty array */
-dyn_array *make_dyn_array(int size);
+dyn_array_heap *make_dyn_array_h(int size);
 /* Not implemented yet */
-void trim_to_size();
+void trim_to_size_h();
 /* Maybe implemented */
-void ensure_capacity(dyn_array *array, int capacity);
+void ensure_capacity_h(dyn_array *array, int capacity);
 /* Implemented */
-dyn_array *resize_array(dyn_array *array, int new_size);
+dyn_array_heap *resize_array_h(dyn_array *array, int new_size);
 /* Implemented */
-//dyn_array *add_element_to_end(dyn_array *array_to_insert_in, element elem_to_insert);
+dyn_array_heap *add_heap_to_end_h(dyn_array *array_to_insert_in, heap_elem heap_to_insert);
 /* Implemented */
-//dyn_array *delete_element(dyn_array *array, element elem_to_delete);
+dyn_array_heap *delete_heap_h(dyn_array *array, heap_elem heap_to_delete);
 /* Not implemented yet */
-//dyn_array *find_element(dyn_array *array, element elem_to_find);
+dyn_array_heap *find_heap_h(dyn_array *array, heap_elem heap_to_find);
+
+/* NODE FUNCTIONS */
+/* Makes a empty array */
+dyn_array *make_dyn_array_n(int size);
+/* Not implemented yet */
+void trim_to_size_n();
+/* Maybe implemented */
+void ensure_capacity_n(dyn_array *array, int capacity);
+/* Implemented */
+dyn_array *resize_array_n(dyn_array *array, int new_size);
+/* Implemented */
+dyn_array *add_node_to_end_n(dyn_array *array_to_insert_in, node node_to_insert);
+/* Implemented */
+dyn_array *delete_node_n(dyn_array *array, node node_to_delete);
+/* Not implemented yet */
+dyn_array *find_node_n(dyn_array *array, node node_to_find);
