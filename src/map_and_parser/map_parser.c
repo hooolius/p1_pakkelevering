b4e48a2251c11b34cca7827357c8f8918b0c0b37
dkalaxdk <alax0013@gmail.com>
2018-12-07 10:12:22 +0100

    Temp push. Debugging.

--- src/map_and_parser/map_parser.c
+++ src/map_and_parser/map_parser.c
@@ -26,11 +26,13 @@ int binary_searcher(double input, struct point points_array[], int array_len);
 
 int points_counter(cJSON *pJSON);
 
-void pointer_writer(struct point points_array[], int j, int j_old);
+void pointer_writer(struct point points_array[], int array_key, int old_array_key);
 
-int is_written(struct point point, int j, int j_old);
+int is_written(struct point point, int array_key, int old_array_key);
 
 int main(void) {
+    /*Terms:
+     *  Points: A representation of a geographical point.*/
     cJSON *json = NULL;
     struct point *points_array;
     int i = 0;
@@ -39,7 +41,7 @@ int main(void) {
     FILE *paddress_file;
     paddress_file = fopen("map_data.json", "rb");
 
-
+    /*Start of standard modification of file to make it capable of JSON parsing*/
     if (paddress_file == NULL) {
         printf("SHIT");
         exit(-1);
@@ -51,8 +53,9 @@ int main(void) {
 
     data = (char *) malloc(len + 1);
     fread(data, 1, len, paddress_file);
+    /*End of standard modification of file to make it capable of JSON parsing*/
 
-
+    /*Calls the cJSON libary parser function*/
     json = cJSON_Parse(data);
 
     if (json == NULL) {
@@ -60,13 +63,12 @@ int main(void) {
         exit(-1);
     } else {
 
-        /*Counts the amount of points within the file*/
+        /*Counts the amount of elements within the file*/
         i = points_counter(json);
         points_array = calloc(i, sizeof(points_array[0]));
 
+        /*Calls the parsing functions*/
         search_and_parse_points(data, points_array);
-
-        printf("Points array: %lf", points_array[0].id);
         search_and_parse_streets(data, points_array);
     }
 
@@ -83,6 +85,7 @@ void search_and_parse_points(char data[], struct point points_array[]) {
 
     points = cJSON_GetObjectItemCaseSensitive(json, "points");
     i = points_counter(json);
+    /*Uses cJSON libary function to construct an array of all JSON points (Points being coordinates)*/
 
     cJSON_ArrayForEach(point, points) { ;
         points_array[j].id = cJSON_GetObjectItemCaseSensitive(point, "id")->valuedouble;
@@ -90,11 +93,13 @@ void search_and_parse_points(char data[], struct point points_array[]) {
         points_array[j].lon = cJSON_GetObjectItemCaseSensitive(point, "lon")->valuedouble;
         j++;
     }
+    /*Sorts the array of points to make searching faster later.*/
     qsort(points_array, i, sizeof(points_array[0]), sorter_function);
 
 }
 
 int points_counter(cJSON *pJSON) {
+    /*Counts points within the JSON file*/
     const cJSON *points = NULL;
     const cJSON *point = NULL;
     int i;
@@ -106,6 +111,7 @@ int points_counter(cJSON *pJSON) {
 }
 
 int sorter_function(const void *a, const void *b) {
+    /*Qsort helper function.*/
     struct point *ia = (struct point *) a;
     struct point *ib = (struct point *) b;
     if (ia->id > ib->id) {
@@ -119,12 +125,13 @@ int sorter_function(const void *a, const void *b) {
 
 
 void search_and_parse_streets(char *data, struct point points_array[]) {
+    /*Function passes streets, and connects the points, so all the points know what points they are connected to.*/
     cJSON *json = NULL;
     const cJSON *road_points = NULL;
     const cJSON *road_point = NULL;
     const cJSON *roads = NULL;
     const cJSON *road = NULL;
-    int array_len = 0, j = 0, j_old = 0;
+    int array_len = 0, array_key = 0, old_array_key = 0;
     double input;
 
     json = cJSON_Parse(data);
@@ -134,120 +141,109 @@ void search_and_parse_streets(char *data, struct point points_array[]) {
 
     } else {
         array_len = points_counter(json);
+        /*Itterates through the JSON file*/
         roads = cJSON_GetObjectItemCaseSensitive(json, "roads");
         cJSON_ArrayForEach(road, roads) {
-
+            /*Finds the elements within the JSON file, and links points.*/
             road_points = cJSON_GetObjectItemCaseSensitive(road, "nodes");
-            //array_len = cJSON_GetArraySize(road_point);
             cJSON_ArrayForEach(road_point, road_points) {
-
-                input = road_point->valuedouble;
-                j = binary_searcher(input, points_array, array_len);
-                if (j_old !=0) {
-                    pointer_writer(points_array, j, j_old);
-                    printf("\t Test of return  ID:: %d Array key: %lf \nJ_Old: %d", j, input, j_old);
-                    printf("\n P1: %d \n P2: %d \n P3: %d \n P4: %d \n P5: %d \n", points_array[j].p1, points_array[j].p2,
-                           points_array[j].p3, points_array[j].p4, points_array[j].p5);
+                /*To make sure all points are connected the following steps are done:
+                 * Binary_search outputs the corresponding array__key to the id of the point.
+                 * If the old_array_key == 0, the array_key is the first within the road.
+                 * */
+                array_key = binary_searcher(road_point->valuedouble, points_array, array_len);
+                if (old_array_key != 0) {
+                    pointer_writer(points_array, array_key, old_array_key);
+                    printf("\t Test of return  ID:: %d Array key: %.0lf \nJ_Old: %d", array_key,
+                           road_point->valuedouble, old_array_key);
+                    printf("\n P1: %d \n P2: %d \n P3: %d \n P4: %d \n P5: %d \n", points_array[array_key].p1,
+                           points_array[array_key].p2,
+                           points_array[array_key].p3, points_array[array_key].p4, points_array[array_key].p5);
                 }
-                j_old = j;
+                old_array_key = array_key;
 
             }
 
-            j_old = 0;
+            old_array_key = 0;
             printf("End of section \n");
         }
     }
 }
 
-void pointer_writer(struct point points_array[], int j, int j_old) {
-    if (!is_written(points_array[j], j, j_old)) {
-        if (points_array[j].p1 == 0) {
-            points_array[j].p1 = j;
+void pointer_writer(struct point points_array[], int array_key, int old_array_key) {
+    /*Inputs the array_key and old_array key into the points.
+     * The points gets connected to eachoter, by writing the array key into the struct,
+     * of the points to which its connected.*/
+    if (!is_written(points_array[array_key], array_key, old_array_key)) {
 
-        } else if (points_array[j].p2 == 0) {
-            points_array[j].p2 = j;
+        if (points_array[array_key].p1 == 0) {
+            points_array[array_key].p1 = old_array_key;
 
-        } else if (points_array[j].p3 == 0) {
-            points_array[j].p3 = j;
+        } else if (points_array[array_key].p2 == 0) {
+            points_array[array_key].p2 = old_array_key;
 
-        } else if (points_array[j].p4 == 0) {
-            points_array[j].p4 = j;
+        } else if (points_array[array_key].p3 == 0) {
+            points_array[array_key].p3 = old_array_key;
 
-        } else if (points_array[j].p5 == 0) {
-            points_array[j].p5 = j;
+        } else if (points_array[array_key].p4 == 0) {
+            points_array[array_key].p4 = old_array_key;
 
-        } else if (points_array[j].p6 == 0) {
-            points_array[j].p6 = j;
+        } else if (points_array[array_key].p5 == 0) {
+            points_array[array_key].p5 = old_array_key;
+
+        } else if (points_array[array_key].p6 == 0) {
+            points_array[array_key].p6 = old_array_key;
+
         }
+    }
 
-        if (j_old != 0) {
-            if (points_array[j_old].p1 == 0) {
-                points_array[j_old].p1 = j;
+    if (!is_written(points_array[old_array_key], array_key, old_array_key)) {
+        if (points_array[old_array_key].p1 == 0) {
+            points_array[old_array_key].p1 = array_key;
 
-            } else if (points_array[j_old].p2 == 0) {
-                points_array[j_old].p2 = j;
+        } else if (points_array[old_array_key].p2 == 0) {
+            points_array[old_array_key].p2 = array_key;
 
-            } else if (points_array[j_old].p3 == 0) {
-                points_array[j_old].p3 = j;
+        } else if (points_array[old_array_key].p3 == 0) {
+            points_array[old_array_key].p3 = array_key;
 
-            } else if (points_array[j_old].p4 == 0) {
-                points_array[j_old].p4 = j;
+        } else if (points_array[old_array_key].p4 == 0) {
+            points_array[old_array_key].p4 = array_key;
 
-            } else if (points_array[j_old].p5 == 0) {
-                points_array[j_old].p5 = j;
+        } else if (points_array[old_array_key].p5 == 0) {
+            points_array[old_array_key].p5 = array_key;
 
-            } else if (points_array[j_old].p6 == 0) {
-                points_array[j_old].p6 = j;
-            }
-
-            if (points_array[j].p1 == 0) {
-                points_array[j].p1 = j_old;
-
-            } else if (points_array[j].p2 == 0) {
-                points_array[j].p2 = j_old;
-
-            } else if (points_array[j].p3 == 0) {
-                points_array[j].p3 = j_old;
-
-            } else if (points_array[j].p4 == 0) {
-                points_array[j].p4 = j_old;
-
-            } else if (points_array[j].p5 == 0) {
-                points_array[j].p5 = j_old;
-
-            } else if (points_array[j].p6 == 0) {
-                points_array[j].p6 = j_old;
-            }
+        } else if (points_array[old_array_key].p6 == 0) {
+            points_array[old_array_key].p6 = array_key;
         }
     }
 }
 
-int is_written(struct point point, int j, int j_old) {
-    if (point.p1 == j) {
+int is_written(struct point point, int array_key, int old_array_key) {
+    /*Checks if the point is already written into the points.*/
+    if (point.p1 == array_key) {
         return 1;
-    } else if (point.p2 == j) {
+    } else if (point.p2 == array_key) {
         return 1;
-    } else if (point.p3 == j) {
+    } else if (point.p3 == array_key) {
         return 1;
-    } else if (point.p4 == j) {
+    } else if (point.p4 == array_key) {
         return 1;
-    } else if (point.p5 == j) {
+    } else if (point.p5 == array_key) {
         return 1;
-    } else if (point.p6 == j) {
+    } else if (point.p6 == array_key) {
         return 1;
-    }
-
-    if (point.p1 == j_old) {
+    } else if (point.p1 == old_array_key) {
         return 1;
-    } else if (point.p2 == j_old) {
+    } else if (point.p2 == old_array_key) {
         return 1;
-    } else if (point.p3 == j_old) {
+    } else if (point.p3 == old_array_key) {
         return 1;
-    } else if (point.p4 == j_old) {
+    } else if (point.p4 == old_array_key) {
         return 1;
-    } else if (point.p5 == j_old) {
+    } else if (point.p5 == old_array_key) {
         return 1;
-    } else if (point.p6 == j_old) {
+    } else if (point.p6 == old_array_key) {
         return 1;
     }
     return 0;
