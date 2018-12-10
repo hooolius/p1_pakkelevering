2ae5507acf71ed2d88aab2112a6904a1ccdad342
sejbas <sejbas@users.noreply.github.com>
2018-12-10 14:35:07 +0100

    Work done to make converter work

--- src/libs/testconverter.c
+++ src/libs/testconverter.c
@@ -23,21 +23,27 @@ double converter(struct gps_point *arr_punkter);
 
 int main(int argc, char **argv) {
 	/*array af punkter fra A* med calloc*/
-	struct gps_point *ptrarr_punkter=calloc(ANTALPUNKTER,sizeof(struct gps_point));
+	//struct gps_point *ptrarr_punkter=calloc(ANTALPUNKTER,sizeof(struct gps_point));
+
+	node_to_gps_point_converter();
+
+	struct gps_point *ptrarr_punkter = a_star(start, slut);
 	if(ptrarr_punkter == NULL)
 	{
 		printf("Error! memory not allocated.");
 		exit(-1);
 	}
 
-	/*test punkter*/
+	/*test punkter
 	ptrarr_punkter[0].latitude = 57.048270;
 	ptrarr_punkter[0].longitude = 9.863372;
 	ptrarr_punkter[1].latitude = 57.046114;
-	ptrarr_punkter[1].longitude = 9.867273;
-	double afstantest= converter(ptrarr_punkter);
+	ptrarr_punkter[1].longitude = 9.867273;*/
+	double afstantest = converter(ptrarr_punkter);
 	printf("test afstand %lf", afstantest);
-	
+
+	matrice_maker();
+
 	free(ptrarr_punkter);
 	return 0;
 }
@@ -46,9 +52,13 @@ double converter(struct gps_point *arr_punkter){
 	int i;
 	/*resultat*/
 	double res=0;
-	for (i = 0; i < ANTALPUNKTER-1; ++i) {
+	for (i = 0; i < ANTALPUNKTER - 1; ++i) {
 		/*Beregn afstanden mellem to punkter og summere til res*/
 		res+=calculate_distance_to_points_3(&arr_punkter[i],&arr_punkter[i++]);
 	}
 	return res;
+}
+
+void matrice_maker() {
+
 }
