f466031b816d16aacd0aa81b2b2d1e1cde1ef3e7
Julius Nyborg Olesen <hoolius.o@gmail.com>
2018-12-10 12:15:56 +0100

    finishing up pairing heap (hopefully)

--- src/pairing_heap.c
+++ src/pairing_heap.c
@@ -3,7 +3,7 @@
 #include "pairing_heap.h"
 #include "dynamic_array.h"
 
-pairing_heap merge_pairs(pairing_heap heap, pairing_heap subheaps[]);
+pairing_heap merge_pairs(pairing_heap heap, dyn_array_heap subheaps[]);
 
 pairing_heap heap_init(pairing_heap heap) {
   dyn_array_heap *subheap_arr;
@@ -39,7 +39,7 @@ pairing_heap merge(pairing_heap heap1, pairing_heap heap2) {
     return heap1;
   }
   else if (heap1.proot->value < heap2.proot->value) {
-    heap1.subheaps[++heap1.subheaps->items] = heap2;
+    add_heap_to_end_h(heap1.subheaps, heap2);
     if (heap1.proot->pleft_child == NULL) {
       heap1.proot->pleft_child = heap2.proot;
       return heap1;
@@ -56,7 +56,7 @@ pairing_heap merge(pairing_heap heap1, pairing_heap heap2) {
     }
   }
   else {
-    heap2.subheaps->subheaps[++heap1.subheaps->size_subheaps] = heap1;
+    add_heap_to_end_h(heap2.subheaps, heap1);
     if (heap2.proot->pleft_child == NULL) {
       heap2.proot->pleft_child = heap1.proot;
       return heap2;
@@ -105,19 +105,24 @@ void delete_min(pairing_heap heap) {
     exit(EXIT_FAILURE);
   } 
   else {
-    merge_pairs(heap, heap.subheaps->subheaps);
+    merge_pairs(heap, heap.subheaps);
   }
 }
 
-pairing_heap merge_pairs(pairing_heap heap, pairing_heap subheaps[]) {
-  if (heap.subheaps->size_subheaps == 0) {
+pairing_heap merge_pairs(pairing_heap heap, dyn_array_heap subheaps[]) {
+  pairing_heap sub1, sub2;
+  if (heap.subheaps->items == 0) {
     return heap;
   }
-  else if (heap.subheaps->size_subheaps == 1) {
-    return merge(heap, subheaps[0]);
+  else if (heap.subheaps->items == 1) {
+    return merge(heap, subheaps->heaps[0]);
   }
   else {
-    return merge((subheaps[0], subheaps[1]), merge_pairs(heap, subheaps));
+    sub1 = subheaps->heaps[0];
+    sub2 = subheaps->heaps[1];
+    delete_heap_h(subheaps->heaps, sub1);
+    delete_heap_h(subheaps->heaps, sub2);
+    return merge(merge(sub1, sub2), merge_pairs(heap, subheaps));
   }
 }
 //lav helper function til merge_pairs, så den kører rekursivt?
