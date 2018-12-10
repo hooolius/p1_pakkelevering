3ec1b5affe6b5530df33ef0270502dd0703a8782
sejbas <sejbas@users.noreply.github.com>
2018-12-10 15:40:12 +0100

    Work to converter

--- src/libs/testconverter.c
+++ src/libs/testconverter.c
@@ -21,28 +21,40 @@
 double converter(struct gps_point *arr_punkter);
 
 
-int main(int argc, char **argv) {
+int converter_main(points_address *addresses, node *nodes) {
 	/*array af punkter fra A* med calloc*/
 	//struct gps_point *ptrarr_punkter=calloc(ANTALPUNKTER,sizeof(struct gps_point));
 
-	struct gps_point *ptrarr_punkter = a_star(start, slut);
-	if(ptrarr_punkter == NULL)
-	{
-		printf("Error! memory not allocated.");
-		exit(-1);
+	node_to_gps_point_converter();
+
+	node start;
+	node slut;
+	while (number_of_address) {
+		start.lat = nodes[addresses.closest_node].lat;
+		start.lon = nodes[addresses.closest_node].lon;
+		slut.lat = addresses.lat;
+		slut.lon = addresses.lon;
+
+		struct gps_point *ptrarr_punkter = a_star(start, slut);
+		if(ptrarr_punkter == NULL)
+		{
+			printf("Error! memory not allocated.");
+			exit(-1);
+		}
+
+		/*test punkter
+		ptrarr_punkter[0].latitude = 57.048270;
+		ptrarr_punkter[0].longitude = 9.863372;
+		ptrarr_punkter[1].latitude = 57.046114;
+		ptrarr_punkter[1].longitude = 9.867273;*/
+		double afstantest = converter(ptrarr_punkter);
+		printf("test afstand %lf", afstantest);
+
+		matrice_maker();
+
+		free(ptrarr_punkter);
 	}
 
-	/*test punkter
-	ptrarr_punkter[0].latitude = 57.048270;
-	ptrarr_punkter[0].longitude = 9.863372;
-	ptrarr_punkter[1].latitude = 57.046114;
-	ptrarr_punkter[1].longitude = 9.867273;*/
-	double afstantest = converter(ptrarr_punkter);
-	printf("test afstand %lf", afstantest);
-
-	matrice_maker();
-
-	free(ptrarr_punkter);
 	return 0;
 }
 /*converter input output funtion fra a* til kombinatorisk */
@@ -52,11 +64,25 @@ double converter(struct gps_point *arr_punkter){
 	double res=0;
 	for (i = 0; i < ANTALPUNKTER - 1; ++i) {
 		/*Beregn afstanden mellem to punkter og summere til res*/
-		res+=calculate_distance_to_points_3(&arr_punkter[i],&arr_punkter[i++]);
+		res += vincent_inv_dist(&arr_punkter[i],&arr_punkter[i++]);
 	}
 	return res;
 }
 
 void matrice_maker() {
 
+}
+
+gps_point *node_to_gps_point_converter(node *nodes) {
+	int i = 0, number_of_nodes;
+	while (nodes[i].id != 0) {
+		++number_of_nodes;
+	}
+	gps_point *points = calloc(number_of_nodes, sizeof(gps_point));
+	for (i = 0; i < number_of_nodes; ++i) {
+		points.latitude = nodes.lat;
+		points.longitude = nodes.lon;
+	}
+
+	return points;
 }
