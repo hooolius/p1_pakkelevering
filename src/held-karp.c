6d4a79af2c8ea5453220c4b34da14f1c87d3cc51
Julius Nyborg Olesen <hoolius.o@gmail.com>
2018-12-11 13:24:26 +0100

    held-karp error fixing

--- src/held-karp.c
+++ src/held-karp.c
@@ -4,6 +4,11 @@
 #include <limits.h>
 #include "held-karp.h"
 
+typedef struct subset {
+  int *subsets;
+  int index;
+} subset; 
+
 void setup(int **matrix, int **memo, int start_node, int size);
 void solve(int **matrix, int **memo, int start_node, int size);
 int calc_min_cost(int **matrix, int **memo, int start_node, int size);
@@ -11,28 +16,34 @@ void calc_best_plan(int **matrix, int **memo,
     int start_node, int size, int plan[]);
 int in_subset(int i, int subset);
 int combinations(int r, int n);
-void combinations_subset(int r, int n, int *subsets);
-void combinations_subset_helper(int j, int set,
-    int at, int r, int n, int *subsets);
+void combinations_subset(int r, int n, subset *subsets);
+void combinations_subset_helper(int set,
+    int at, int r, int n, subset *subsets);
 unsigned long factorial(unsigned long i);
 
 int main(void) {
   int **matrix;
-  matrix = calloc(20, sizeof(int*));
+  int n = 10;
+  matrix = calloc(n, sizeof(int*));
   int q;
-  for(q=0;q<20;q++)
-    matrix[q]=(int *) calloc(20, sizeof(int));
+  for(q=0;q<n;q++)
+    matrix[q]=(int *) calloc(n, sizeof(int));
 
   int x, y;
-  for(x = 0; x < 20; x ++) {
-    for(y = 0; y < 20; y ++) matrix[x][y] = x+y;
+  for(x = 0; x < n; x ++) {
+    for(y = 0; y < n; y ++) matrix[x][y] = x+y;
   }
   int min_cost = 0;
   int start_node = 0;
-  int a[21];
-  int size = sizeof(a) / sizeof(int);
+  int a[n+1];
+  int size = (sizeof(a) / sizeof(int)) - 1;
+
+  for (int i = 0; i < size; i++) {
+    a[i] = i*3;
+  }
+
   held_karp(matrix, size, start_node, &min_cost, a);
-  printf("min cost: %d, optimal route: ", min_cost);
+  printf("min cost: %d, optimal route: \n", min_cost);
   for (int i = 0; i < size; i++) {
     printf("%d, ", a[i]);
   }
@@ -44,8 +55,8 @@ void held_karp(int **matrix, int size,
   int i; 
   int **memo; 
   memo = calloc(size, sizeof(int*));
-  for(i=0;i<size;i++) {
-    matrix[i]=(int *) calloc((int) pow(2, size), sizeof(int));
+  for(i = 0; i < size; i++) {
+    memo[i]=(int *) calloc((int) pow(2, size), sizeof(int));
   }
   setup(matrix, memo, start_node, size);
   solve(matrix, memo, start_node, size);
@@ -61,40 +72,47 @@ void setup(int **matrix, int **memo, int start_node, int size) {
   for (int i = 0; i < size; i++) {
     if (i != start_node) {
       memo[i][1 << start_node | 1 << i] = matrix[start_node][i];
+      //printf("memo %d: %d\n", i, matrix[start_node][i]);
     }
   }
 }
 
 void solve(int **matrix, int **memo, int start_node, int size) {
-  for (int r = 3; r < size; r++) {
-    int *subsets;
-    subsets = calloc(combinations(r, size), sizeof(int));
+  for (int r = 3; r <= size; r++) {
+    subset asubsets;
+    subset *subsets = &asubsets;
+    subsets->index = 0;
+    subsets->subsets = calloc(combinations(r, size), sizeof(int));
+    combinations_subset(r, size, subsets);
     for (int i = 0; i < combinations(r, size); i++) {
-      if (in_subset(start_node, subsets[i])) {
+      if (in_subset(start_node, subsets->subsets[i])) {
         for (int next = 0; next < size; next++) {
-          if (next != start_node && in_subset(next, subsets[i])) {
-            int state = subsets[0] ^ (1 << next);
-            int min_dist = INT_MAX;
+          int min_dist = INT_MAX;
+          if (next != start_node && in_subset(next, subsets->subsets[i])) {
+            int state = subsets->subsets[i] ^ (1 << next);
+            printf("%d, i %d\n", state, next);
             for (int end = 0; end < size; end++) {
               if (end != start_node && end != next
-                  && in_subset(end, subsets[i])) {
+                  && in_subset(end, subsets->subsets[i])) {
                 int new_dist = memo[end][state] + matrix[end][next];
+                //printf("%d\n", new_dist);
                 if (new_dist < min_dist) {
                   min_dist = new_dist;
                 }
               }
             }
-            memo[next][subsets[0]] = min_dist;
           }
+          memo[next][subsets->subsets[i]] = min_dist;
         }
       }
     }
+    free(subsets->subsets);
   }
 }
 
 /* checks whether or not bit i in the subset is a 1 and returns a bool */
 int in_subset(int i, int subset) {
-  return ((1 << i) & subset) == 1;
+  return !(((1 << i) | subset) == 0);
 }
 
 int combinations(int r, int n) {
@@ -110,20 +128,22 @@ unsigned long factorial(unsigned long i) {
   }
 }
 
-void combinations_subset(int r, int n, int *subsets) {
-  combinations_subset_helper(0, 0, 0, r, n, subsets);
+void combinations_subset(int r, int n, subset *subsets) {
+  combinations_subset_helper(0, 0, r, n, subsets);
 }
 
-void combinations_subset_helper(int j, int set,
-    int at, int r, int n, int *subsets) {
+void combinations_subset_helper(int set,
+    int at, int r, int n, subset *subsets) {
   if (r == 0) {
-    subsets[j] = set;
+    subsets->subsets[subsets->index] = set;
+    //printf("index: %d set: %d \n", subsets->index, set);
+    subsets->index++;
   }
   else {
     for (int i = at; i < n; i++) {
       set = set | (1 << i);
 
-      combinations_subset_helper(j, set, i + 1, r - 1, n, subsets);
+      combinations_subset_helper(set, i + 1, r - 1, n, subsets);
 
       set = set & ~(1 << i);
     }
