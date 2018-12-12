e899b6973a8bda00b78ab4cf3efadd00c1c0a31e
sejbas <markamb2497@gmail.com>
2018-12-12 09:32:38 +0100

    Bug fixingV2.exe

--- src/dynamic_array.h
+++ src/dynamic_array.h
@@ -31,14 +31,14 @@ dyn_array_heap *make_dyn_array_n(int size);
 void trim_to_size_n();
 
 /* Maybe implemented */
-void ensure_capacity_n(dyn_array_heap *array, int capacity);
+void ensure_capacity_n(dyn_array_node *array, int capacity);
 
 /* Implemented */
-dyn_array_heap *resize_array_n(dyn_array_heap *array, int new_size);
+dyn_array_heap *resize_array_n(dyn_array_node *array, int new_size);
 
 /* Implemented */
-dyn_array_heap *add_heap_to_end_n(dyn_array_heap *array_to_insert_in, pairing_heap heap_to_insert);
+dyn_array_heap *add_node_to_end_n(dyn_array_node *array_to_insert_in, node node_to_insert);
 /* Implemented */
-dyn_array_heap *delete_heap_n(dyn_array_heap *array, pairing_heap heap_to_delete);
+dyn_array_heap *delete_node_n(dyn_array_node *array, node node_to_delete);
 /* Not implemented yet */
-dyn_array_heap *find_heap_n(dyn_array_heap *array, pairing_heap heap_to_find);
+dyn_array_heap *find_node_n(dyn_array_node *array, node node_to_find);
