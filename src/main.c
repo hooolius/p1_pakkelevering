072a6f9e997779019922db76cd991d43e48e692b
Alexander Nykjær <alax0013@gmail.com>
2018-12-17 23:10:51 +0100

    Tried to do Cmake for windows... didnt work

--- src/main.c
+++ src/main.c
@@ -11,6 +11,18 @@
 #include "combinatorial_optimisation/held_karp.h"
 #include "combinatorial_optimisation/ioconverter.h"
 #include <time.h>
+
+#if defined(__linux) || defined(__linux__) || defined(linux)
+# define LINUX
+  
+#elif defined(__APPLE__)
+# define MACOS
+  
+#elif defined(_WIN32) || defined(__WIN32__) || defined(WIN32) || defined(_WIN64)
+# define WINDOWS
+  
+#endif
+
 typedef struct point point;
 
 
