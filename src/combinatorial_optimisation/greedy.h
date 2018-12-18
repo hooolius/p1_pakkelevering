f2df1dc51808b9a00a746a80fa33efa8d9683131
sejbas <markamb2497@gmail.com>
2018-12-18 22:08:48 +0100

    Cleanup in greedy.h and ioconverter.h

--- src/combinatorial_optimisation/greedy.h
+++ src/combinatorial_optimisation/greedy.h
@@ -1,2 +1,2 @@
-//GREEEEEEEEEEEEEEEEED
+//This function is only used for testing purposes
 int greedy(int **matrix, int size);
--- src/combinatorial_optimisation/ioconverter.h
+++ src/combinatorial_optimisation/ioconverter.h
@@ -5,8 +5,8 @@
  * prototypes
 /**
  *main function in input output converter:
- * @param[in] a dynamic array of adresses
- * @param[in] pointer of type point
- * @return 2D integer distance matrix
+ * @param[in] "*searches" is a dynamic array of addresses
+ * @param[in] "*map_points" is a pointer of type point
+ * @return "afstand_matrix" 2D integer distance matrix
  */
 int **astar_to_matrix_converter(dyn_array_address *searches, struct point *map_points);
