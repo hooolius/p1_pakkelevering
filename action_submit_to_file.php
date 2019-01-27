993ac48b1e3dd65635014d29e9934deaddddccf6
Alexander Nykjær <alax0013@gmail.com>
2019-01-27 13:03:12 +0100

    Tweaked minor changes to make code easier, and added wait for updated file.

--- action_submit_to_file.php
+++ action_submit_to_file.php
@@ -7,22 +7,22 @@ if (isset($_SESSION['time'])) {
 }
 
 
-
-
 $_SESSION['time'] = $time_stamp;
-$path = "searches/".$time_stamp."_input.txt";
+$path = "searches/" . $time_stamp . "_input.txt";
 if (isset($_POST['addresses_input'])) {
     $fh = fopen($path, "a+");
     $string = $_POST['addresses_input'];
     fwrite($fh, $string); // Write information to the file
     fclose($fh); // Close the file
 
-    if(file_exists("p1_pakkelevering.exe")) {
-        exec("p1_pakkelevering.exe data.txt");
-    } elseif (file_exists("p1_pakkelevering.out")){
-        exec("p1_pakkelevering.out data.txt");
+    if (file_exists("p1_pakkelevering.exe") OR file_exists("p1_pakkelevering.out")) {
+        exec("p1_pakkelevering.out" . $time_stamp . "txt >>" . $time_stamp . "_output.txt");
     } else {
         echo "<br> No executable found.";
+    }
+
+    while (file_exists("searches/".$time_stamp."_output.txt")) {
+        header("location: index.php");
     }
 }
 
